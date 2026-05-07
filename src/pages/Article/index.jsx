339372e9bd853e99a8a2de7b615fde813c2f9e54
庞玮蕊 <2678327742@qq.com>
2026-05-07 19:56:54 +0800

    feat: 筛选功能实现

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -77,14 +77,35 @@ const Article = () => {
   ///获取文章列表
   const [list,setList]=useState([])
   const [count,setCount]=useState(0)
+  //筛选参数
+  const [reqData,setReqData]=useState(
+    {
+      status:'',
+      channel_id:'',
+      begin_pubdate:'',
+      end_pubdate:'',
+      page:1,
+      page_size:4
+    }
+  )
+  //筛选
+  const onFinish=(formValue)=>{
+    setReqData({
+      ...reqData,
+      channel_id:formValue.channel_id,
+      status:formValue.status,
+      begin_pubdate:formValue.date[0].format('YYYY-MM-DD'),
+      end_pubdate:formValue.date[1].format('YYYY-MM-DD'),
+    })
+  }
   useEffect(()=>{
     const getList=async()=>{
-      const res=await getArticleListAPI()
+      const res=await getArticleListAPI(reqData)
       setList(res.data.results)
       setCount(res.data.total_count)
     }
     getList()
-  })
+  },[reqData])
   return (
     <div>
       <Card
@@ -96,7 +117,7 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: '' }}>
+        <Form initialValues={{ status: '' }} onFinish={onFinish}>
           <Form.Item label="状态" name="status">
             <Radio.Group>
               <Radio value={''}>全部</Radio>
